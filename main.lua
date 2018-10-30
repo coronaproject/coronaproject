b7797e58b2c53a8a8f44f11eb0a1c1dc12fc98ef
qqwe9987tw9 <qqwe9987tw9@gmail.com>
2018-10-30 08:55:04 +0800

    １０１３
    １０１３

--- main.lua
+++ main.lua
@@ -12,3 +12,5 @@ local msgText = display.newText( "Hello, world!", world.x, world.y+320, native.s
 msgText:setFillColor( 0.1, 0.1, 0.1,)
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
+local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
